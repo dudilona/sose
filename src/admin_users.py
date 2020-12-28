eba958191965829a27871163055d49aebde3d2dc
Anton <247demich@gmail.com>
2020-12-28 20:56:49 +0300

    [feature] Implement the logic of changing the password for the user from the admin panel
    - Implement admin api methods: GET method - for render user change password page
    and POST method for update user hash in the data base
    - Implement the admin user-password.html template

--- src/admin_users.py
+++ src/admin_users.py
@@ -5,6 +5,7 @@ from flask import flash
 from flask import redirect
 from flask import render_template
 from flask import request
+from werkzeug.security import generate_password_hash
 
 from src import apology
 from src.admin_settings import admin_required
@@ -55,19 +56,58 @@ def admin_users_edit():
             is_admin = 1
 
         db = get_db()
-        db.execute("UPDATE user SET username = ?, "
-                   + "fullname = ?, "
-                   + "phone = ?, "
-                   + "email = ?, "
-                   + "address = ?, "
-                   + "is_admin = ?"
-                   + " WHERE id = ?",
-                   (user['username'], user['fullname'], user['phone'], user['email'], user['address'], is_admin,
-                    user['id'])
+        db.execute("UPDATE user SET "
+                   "username = ?, "
+                   "fullname = ?, "
+                   "phone = ?, "
+                   "email = ?, "
+                   "address = ?, "
+                   "is_admin = ?"
+                   " WHERE id = ?",
+                   (user['username'], user['fullname'], user['phone'], user['email'], user['address'], is_admin, user['id'])
                    )
         db.commit()
 
         flash("User data edited successfully")
         return redirect("/admin/users")
+
+    return apology("Method Not Allowed", 405)
+
+
+@bp.route("/admin/users/password", methods=["GET", "POST"])
+@admin_required
+def admin_users_password():
+    """ Admin panel - user edit password"""
+    if request.method == "GET":
+        user_id = request.args.get("userId")
+        db = get_db()
+        user = db.execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+
+        return render_template("admin/user-password.html", user=user)
+
+    if request.method == "POST":
+        user_id = request.form.get("user_id")
+        new_pass = request.form.get("password")
+        confirm_pass = request.form.get("password2")
+
+        # Ensure newPass was submitted
+        if not new_pass:
+            return apology("must provide new password", 400)
+
+        # Ensure confirmPass was submitted
+        if not confirm_pass:
+            return apology("must provide new password confirmation", 400)
+
+        # Check passwords are equal
+        if new_pass != confirm_pass:
+            return apology("The passwords aren't equal", 400)
+
+        # Update the password
+        db = get_db()
+        db.execute("UPDATE user SET hash = ? WHERE id = ?", (generate_password_hash(new_pass), user_id), )
+        db.commit()
+
+        flash('The password is changed')
+        return redirect("/admin/users/edit?userId=" + user_id)
 
     return apology("Method Not Allowed", 405)
